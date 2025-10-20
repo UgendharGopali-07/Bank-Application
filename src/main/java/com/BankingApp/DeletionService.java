e945d0508112fef8ceea3c54b3e218695ef27b53
Gopali Ugendhar <naniugendar@gmail.com>
2025-10-20 09:42:53 +0530

    Add DeletionService for deletion of accounts and users

---
+++ src/main/java/com/BankingApp/DeletionService.java
@@ -0,0 +1,55 @@
+package com.BankingApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeletionService {
+    @Autowired
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+
+    DeletionService(AccountRepository accountRepository,UserRepository userRepository){
+        this.accountRepository=accountRepository;
+        this.userRepository=userRepository;
+    }
+
+    public String deleteUserById(int id){
+        if(userRepository.getUsers().containsKey(id)){
+            User user=userRepository.getUserById(id);
+            for(Account acc:user.getAccountList()){
+                boolean temp=accountRepository.deleteAccount(acc.getAccId());
+                //optional(lambda function):
+                //user.getAccountsList().forEach(acc -> accRepo.deleteAcc(acc.getAccId()));
+                // lambda function can ignore return type
+            }
+            boolean temp= userRepository.deleteUser(id);
+            return "Successfully deleted user.";
+        }
+        return "User deletion failed!";
+    }
+    public String deleteAccountById(int id){
+        if(accountRepository.getAccountList().containsKey(id)){
+            Account account=accountRepository.getAccountList().get(id);
+            User user=userRepository.getUserById(account.getUserId());
+            for(Account acc:user.getAccountList()){
+                if(acc.getAccId()==id){
+                    user.getAccountList().remove(acc);
+                    break;
+                }
+            }
+            for(Account acc:user.getWatchList()){
+                if(acc.getAccId()==id){
+                    user.getWatchList().remove(acc);
+                    break;
+                }
+            }
+            // above code gives ConcurrentException with iteration,so use lambda function:
+            // u.getAccountList().removeIf(a->a.getAccId()==id);
+            // or you can use for-index loop i.e,. for(int i=0;i..) but after deleting do i--; so that we cant next index
+            boolean temp=accountRepository.deleteAccount(id);
+            return "Account deleted successfully.";
+        }
+        return "Account deletion failed!";
+    }
+}
